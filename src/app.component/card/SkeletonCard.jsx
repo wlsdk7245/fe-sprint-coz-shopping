9cd77fafaf8814a937e57a733d68cc9684795a15
jina <wlsdk7245@gmail.com>
2023-05-12 15:23:49 +0900

    feat: 카드리스트, skeleton 구현

---
+++ src/app.component/card/SkeletonCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+
+const SkeletonCard = () => {
+  return (
+    <StyledWrapper>
+      <div className="card-image" />
+    </StyledWrapper>
+  );
+};
+
+export default SkeletonCard;
+
+const StyledWrapper = styled.div`
+  position: relative;
+  width: 264px;
+
+  .card-image {
+    border-radius: 12px;
+    width: 264px;
+    height: 210px;
+    object-fit: cover;
+    cursor: pointer;
+    background-color: var(--color-gray);
+  }
+`;
