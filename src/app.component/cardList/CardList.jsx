bfb0abcc2268ab086408138ffc05e4d5c5505667
jina <wlsdk7245@gmail.com>
2023-05-15 09:25:05 +0900

    feat: 카드리스트 공통컴포넌트 수정 (Skeleton)

---
+++ src/app.component/cardList/CardList.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+import Card from "../card/Card";
+
+const CardList = ({ cardList }) => {
+  return (
+    <StyledWrapper>
+      {cardList.map((product) => {
+        const { id } = product;
+        return (
+          <Card
+            key={`${Date.now()}-${Math.random()}-${id}`}
+            id={id}
+            info={product}
+          />
+        );
+      })}
+    </StyledWrapper>
+  );
+};
+
+export default CardList;
+
+const StyledWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  column-gap: 24px;
+  row-gap: 12px;
+  min-height: 264px;
+`;
