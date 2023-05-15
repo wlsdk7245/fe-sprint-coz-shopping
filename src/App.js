f4dcbec047061b83cd2f6c302d399cbcb70610a3
jina <wlsdk7245@gmail.com>
2023-05-15 09:26:09 +0900

    feat: 북마크 페이지 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Main from "./app.feature/Main/Main";
 import Product from "./app.feature/Product/Product";
+import Bookmark from "./app.feature/Bookmark/Bookmark";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
       <Route path="/products/list" element={<Product />} />
+      <Route path="/bookmark" element={<Bookmark />} />
     </Routes>
   );
 }
